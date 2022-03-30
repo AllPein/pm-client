8b2dd14d0c996072d307782dd93c0b5990d78606
Aleksandr Panin <Aleksandr_Panin@epam.com>
2022-03-30 18:03:41 +0500

    fixed deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var serveStatic = require('serve-static')
 var app = express()
-app.use(serveStatic(__dirname + "/dist"))
+var path = require('path')
+app.use(serveStatic(path.resolve(__dirname + "/dist")))
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
