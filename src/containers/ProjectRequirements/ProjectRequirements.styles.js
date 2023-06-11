3d8dfd036aef5d9f4a795fa0e1a71cf059fa124e
a.o.panin <a.o.panin@tinkoff.ru>
2023-06-11 19:45:59 +0500

    fixed error

--- src/containers/ProjectRequirements/ProjectRequirements.styles.js
+++ src/containers/ProjectRequirements/ProjectRequirements.styles.js
@@ -17,10 +17,16 @@ const InputWrapper = styled.div`
   width: 30rem;
 `
 
-
+const FieldName = styled.h2`
+  margin: 1.2rem 0;
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: ${(props) => props.theme.color.primary4};
+`;
 
 export {
   Wrapper,
   StyledButton,
   InputWrapper,
+  FieldName
 }
