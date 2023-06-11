6c353d8dc0d53a5e2392f7220d1f5a9c964668d5
a.o.panin <a.o.panin@tinkoff.ru>
2023-06-11 19:08:21 +0500

    deploy

--- src/utils/env.js
+++ src/utils/env.js
@@ -5,7 +5,7 @@ const getEnvValue = (name) => (
 const ENV = {
   BACKEND_URL: getEnvValue('REACT_APP_BACKEND_URL'),
   CLOUD_NAME: getEnvValue('REACT_APP_CLOUD_NAME'),
-  UPLOAD_PRESET: getEnvValue('REACT_APP_UPLOAD_PREST')
+  UPLOAD_PRESET: getEnvValue('REACT_APP_UPLOAD_PRESET')
 }
 
 export {
